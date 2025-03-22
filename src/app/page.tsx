c40ddf159a45f53e723ba646656d0c8f2c9ef9c6
GabrielCorreiadev <seu-email@gmail.com>
2025-03-22 20:19:23 -0300

    Primeiro commit do SMVI

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">Bem-vindo ao SMVI</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        Sistema de Monitoramento de Violência Contra o Idoso
+      </p>
+    </div>
+  );
+}
