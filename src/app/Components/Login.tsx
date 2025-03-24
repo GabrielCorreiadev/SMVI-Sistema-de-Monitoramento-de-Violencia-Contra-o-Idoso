8cdcb7d9573c1d0d9e60d99e9da83d041fbbd929
GabrielCorreiadev <seu-email@gmail.com>
2025-03-24 17:01:58 -0300

    Adiciona animação de saída suave antes da tela de login

---
+++ src/app/Components/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-800 text-white">
+        <h1 className="text-3xl">Tela de Login</h1>
+      </div>
+    );
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,44 @@
 "use client";
 
-import { Shield } from 'lucide-react';
+import { Shield, Timer } from 'lucide-react';
 import { motion } from "framer-motion";
+import { useState, useEffect} from "react"
+import  Login from './Components/Login';
 
 
-export default function Homepage() {
+export default function Homepage()   {
+
+  const [showLogin, setShowLogin] = useState(false);
+  const [isExiting, setIsExiting] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsExiting(true);   
+
+      setTimeout(() => {
+        setShowLogin(true);
+      }, 1500); 
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if(showLogin){
+    return <Login/>;
+  }
+
+
+
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
       <motion.h1
         initial={{ opacity: 0, y: -50 }} // Começa invisível e sobe
-        animate={{ opacity: 1, y: 0 }} // Fica visível e desce suavemente
-        transition={{ duration: 1.5, ease: "easeOut" }} // Suavização
+        animate={isExiting? {opacity: 0, y: 50} : { opacity: 1, y: 0 }} 
+        transition={{ duration: 1.5, ease: "easeOut" }} 
         className="text-9xl font-extrabold text-white flex items-center gap-4"
       >
-        <Shield className="h-24 w-24" /> SMVI
+        <Shield className="h-40 w-30" /> SMVI
       </motion.h1>
     </div>
   );
